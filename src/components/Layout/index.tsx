24bb51b9eddb68f5e6170b3e2505079555fbd6a4
Mário Prada <marioprada@Marios-MBP.lan>
2020-09-20 18:51:47 +0100

    adding

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,1 +1,16 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import theme from '../../theme/theme';
+import * as S from './Layout.styled'
+
+const Layout = () => {
+    return (
+        <ThemeProvider theme={theme}>
+            <S.Container>
+                <h1>ola</h1>
+            </S.Container>
+        </ThemeProvider>
+    )
+}
+
+export default Layout
