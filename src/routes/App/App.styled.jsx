0b3c84bb85ce38dc153a0454015cdab9508dd53f
mariosilvaprada@gmail.com <mariosilvaprada@gmail.com>
2020-09-20 23:26:58 +0100

    working on table

--- src/routes/App/App.styled.jsx
+++ src/routes/App/App.styled.jsx
@@ -8,22 +8,20 @@ export const StyledTable = styled.table`
     color: white;
     border: 1px solid white;
     border-collapse: collapse;
-`
+`;
 
 export const Row = styled.tr`
 
-`
+`;
 
 export const TableHeader = styled.th`
     padding: 1rem;
     border: 1px solid white;
     font-size: 1.1rem;
     text-align: start;
-`
+`;
 
 export const TableData = styled.td`
    padding: 1rem;
    border: 1px solid white;
-`
-
-
+`;
