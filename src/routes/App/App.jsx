2c3d73642f65847966695d414c3cf3c8ad18dd5a
Mário Prada <marioprada@Marios-MBP.lan>
2020-09-21 10:46:17 +0100

    updating

--- src/routes/App/App.jsx
+++ src/routes/App/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from 'components/Layout/index.tsx';
-
+import { BsQuestionCircleFill } from 'react-icons/bs';
 import Table from '../../components/Table/index.tsx';
 import * as S from './App.styled';
 
@@ -17,30 +17,37 @@ const App = () => {
     {
       fieldName: 'Operating Cash Flow',
       fieldType: 'number',
+      tip: true,
     },
     {
       fieldName: 'Total Debt (Short term + Long Term)',
       fieldType: 'number',
+      tip: true,
     },
     {
       fieldName: 'Cash and Short term investments',
       fieldType: 'number',
+      tip: true,
     },
     {
       fieldName: 'Cash flow growth rate (Yr 1 - 3)',
       fieldType: 'number',
+      tip: true,
     },
     {
       fieldName: 'Cash flow growth rate (Yr 4 - 10)',
       fieldType: 'number',
+      tip: true,
     },
     {
       fieldName: 'No of shares Outstanding',
       fieldType: 'number',
+      tip: true,
     },
     {
       fieldName: 'Discount Rate',
       fieldType: 'number',
+      tip: true,
     },
   ];
 
@@ -48,9 +55,16 @@ const App = () => {
     <Layout>
       <S.Title>Home</S.Title>
       <S.InputWrapper>
-        {fieldsInputs.map(({ fieldName, fieldType }) => (
+        {fieldsInputs.map(({ fieldName, fieldType, tip }) => (
           <S.RowInput>
-            <S.Label>{fieldName}</S.Label>
+            <S.Label>
+              <p>{fieldName}</p>
+              {tip && (
+                <S.Tip>
+                  <BsQuestionCircleFill />
+                </S.Tip>
+              )}
+            </S.Label>
             <S.StyledInput type={fieldType} />
           </S.RowInput>
         ))}
